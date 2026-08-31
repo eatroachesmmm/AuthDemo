a76f1839061944b927a7bc022199b40e26b2e90c
eatroachesmmm <timurten288@gmail.com>
2026-08-31 10:04:35 -0500

    Initial commit

---
+++ LoginApi/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+using LoginApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoginApi.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+}
---
+++ LoginApi/Program.cs
@@ -0,0 +1,44 @@
+using LoginApi.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+app.UseCors("AllowFrontend");
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/users", async (AppDbContext db) =>
+{
+    var users = await db.Users.ToListAsync();
+
+    return users;
+});
+
+app.Run();
